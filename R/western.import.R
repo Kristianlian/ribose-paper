8b87d72a315fc59faca3dd2005fa922da4a6c6d0
dhammarstrom <d.hammarstrom@hotmail.se>
2023-01-24 10:38:27 +0100

    updt west

--- R/western.import.R
+++ R/western.import.R
@@ -99,10 +99,10 @@ mean.tp <- western.dat %>%
               values_from = mean_gray) %>%
   ## Lane normalization factor: TPS for each lane (tpl) divided by TPS Signal from lane with highest TPS signal
   mutate(tpl = tp - bg) %>%
-  mutate(tp = tp/mean(tpl)) %>%
-  #print()
-  #mutate(tp = tpl/max(tp)) %>%
-  dplyr::select(gel, sample.id, tp) %>%
+  group_by(gel) %>%
+  mutate(tpl = tpl/max(tpl)) %>%
+
+  dplyr::select(gel, sample.id, tpl) %>%
   print()
 
 
@@ -120,7 +120,7 @@ dat2 <- western.dat %>%
   mutate(target = gsub(".sig", "", target), 
          target = gsub("2", "", target)) %>%
   
-  group_by(gel, sample.id, subject, leg, time, target) %>%
+  group_by(gel, sample.id, sample.name, subject, leg, time, target) %>%
   summarise(signal = mean(signal, na.rm = TRUE)) %>%
   
   inner_join(mean.tp) %>%
@@ -129,12 +129,14 @@ dat2 <- western.dat %>%
   
   group_by(gel, target) %>%
   
-  #mutate(tp = tp / max(tp), 
-        # norm.sign = signal / tp) %>%
-  mutate(norm.sign = signal / tp) %>%
+  mutate(signal = signal / max(signal)) %>%
+  
+  ungroup() %>%
+
+  mutate(norm.sign = signal) %>%
   
   # Adds pool on all samples per gel
-  group_by(gel) %>%
+  group_by(gel, target) %>%
   mutate(pool = if_else(subject == "pool", norm.sign, NA_real_), 
          pool = mean(pool, na.rm = TRUE)) %>%
   ungroup() %>%
@@ -147,11 +149,67 @@ dat2 <- western.dat %>%
   
   group_by(subject) %>%
   mutate(gel.sorted = if_else(gel == max(gel), "B", "A"), 
-         time = factor(time, levels = c("pre", "post"))) %>%
+         time = factor(time, levels = c("pre", "post")), 
+         gel.sample = paste0(gel.sorted, sample.id)) %>%
+  
+  ## Add code key data 
+  mutate(sample.time = gsub("pro1", "", sample.name)) %>%
+
+  inner_join(read_excel("data/code_key.xlsx") %>%
+               mutate(sample.time = time, 
+                      subject = as.character(subject)) %>%
+               dplyr::select(subject, supplement, sample.time)) %>%
+  dplyr::select(gel, sample.id, gel.sorted, subject, leg, time, target, supplement, tpl, norm.sign) %>%
+  mutate(gel.sample = paste0(gel, sample.id), 
+         supplement = factor(supplement, levels = c("PLACEBO", "GLUCOSE"))) %>%
 
  
   print()
 
+
+
+
+dat2 %>%
+  
+  dplyr::select(gel.sorted, sample.id, target, norm.sign) %>%
+  pivot_wider(names_from = gel.sorted, values_from = norm.sign) %>%
+  ggplot(aes(log(A), log(B))) + geom_point() + facet_wrap(~target)
+  
+  
+library(nlme); library(emmeans)
+
+
+
+m0 <- lme(log(norm.sign) ~ time + supplement + time:supplement, 
+         random = list(subject = ~ 1),
+ 
+       
+         data = filter(dat2, target == "cmyc"))
+
+m1 <- lme(log(norm.sign) ~ tpl + target + target:time + target:supplement + target:time:supplement, 
+         random = list(subject = ~ 1, 
+                       gel.sample = ~ 1),
+         weights = varExp(form = ~ tpl|target),
+         method = "ML",
+         data = dat2)
+
+
+anova(m0, m1)
+
+
+
+summary(m)
+
+plot(m0, resid(., type = "p") ~ fitted(.))
+
+
+
+emmeans(m0, specs = ~ time|supplement) %>%
+  data.frame() %>%
+  mutate(time = factor(time, levels = c("pre", "post"))) %>%
+  ggplot(aes(time, emmean, fill = supplement, group = supplement)) + 
+  geom_line() + 
+  geom_point(shape= 21) 
 
 
 
