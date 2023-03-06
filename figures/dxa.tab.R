73005f422e86220c6fa723bc9c4379fd8a879209
Kristianlian <74902710+Kristianlian@users.noreply.github.com>
2023-03-06 18:39:27 +0100

    desc tab prepp

--- figures/dxa.tab.R
+++ figures/dxa.tab.R
@@ -18,10 +18,30 @@ code <- read_excel("./data/sup.xlsx")
 ## Gendercount - from the script dxa.R by SCM
 
 gendercount <- dxa.dat %>%
-  count(sex) %>%
-  pivot_wider(names_from = sex,
-              values_from = n)
+  count(sex)
 
+## Per subject
+
+dxa.dat %>%
+  select(-timepoint) %>%
+  # Convert g to kg
+  mutate(leanmass = leanmassg/1000) %>%
+  select(subject, sex, age, height, weight, leanmass) %>%
+  group_by(sex) %>%
+  summarise(mean.age = mean(age, na.rm = TRUE),
+            mean.height = mean(height, na.rm = TRUE),
+            mean.weight = mean(weight, na.rm = TRUE),
+            mean.leanm = mean(leanmass, na.rm = TRUE),
+            sd.a = sd(age, na.rm = TRUE),
+            sd.h = sd(height, na.rm = TRUE),
+            sd.w = sd(weight, na.rm = TRUE),
+            sd.l = sd(leanmass, na.rm = TRUE)) %>%
+  mutate(a.stat = paste0(round(mean.age, 2), " ± ", round(sd.a, 2)),
+         h.stat = paste0(round(mean.height, 2), " ± ", round(sd.h, 2)),
+         w.stat = paste0(round(mean.weight, 2), " ± ", round(sd.w, 2)),
+         l.stat = paste0(round(mean.leanm, 2), " ± ", round(sd.l, 2))) %>%
+  full_join(gendercount) %>%
+  select(sex, n, a.stat, h.stat, w.stat, l.stat, )
 
 ## Per leg
 
@@ -38,7 +58,18 @@ leg.desc <- leg.dat %>%
             mean.tm = mean(totalmasskg, na.rm = TRUE),
             sd.lm = sd(leanmass, na.rm = TRUE),
             sd.fm = sd(fatmass, na.rm = TRUE),
-            sd.tm = sd(totalmasskg, na.rm = TRUE))
+            sd.tm = sd(totalmasskg, na.rm = TRUE)) %>%
+  mutate(lm.stat = paste0(round(mean.lm, 2), " ± ", round(sd.lm, 2)),
+         fm.stat = paste0(round(mean.fm, 2), " ± ", round(sd.fm, 2)),
+         tm.stat = paste0(round(mean.tm, 2), " ± ", round(sd.tm, 2))) %>%
+  mutate(supplement = if_else(supplement == "glucose",
+                              "Glucose",
+                              if_else(supplement == "placebo",
+                                      "Placebo", supplement))) %>%
+  select(supplement, lm.stat, fm.stat, tm.stat) %>%
+  print()
+
+
 
 
 
