7afd77e8134dcec8ce9a0dd95d6d5f2a80c1612e
Kristianlian <74902710+Kristianlian@users.noreply.github.com>
2023-03-06 16:05:35 +0100

    vol table work

--- figures/vol.tab.R
+++ figures/vol.tab.R
@@ -1,67 +1,119 @@
 ######## Training volume table
 
 # Packages
-library(tidyverse); library(flextable)
+library(tidyverse); library(flextable); library(emmeans)
 
 # Data
 
-vol.abs <- readRDS("./data/data-gen/training/volume.absolute.RDS")
+vol.clean <- readRDS("./data/data-gen/training/vol.clean.RDS")
 
 vol.change <- readRDS("./data/data-gen/training/vol.change.RDS")
+  
+# Flextable for absolute values
 
-# Model
-
-m <- lmerTest::lmer(change ~ 0 + baseline + time + supplement:time + (1|subject),
-                     data = vol.change)
-plot(m)
-msum <- summary(m)
-
-vol.emm <- readRDS("./data/data-gen/training/vol.emm.RDS") %>%
-  select(-SE, -df) %>%
-  mutate(Time = if_else(time == "change.2",
-                        "2",
-                        if_else(time == "change.3",
-                                "3",
-                                if_else(time == "change.4",
-                                        "4",
-                                        if_else(time == "change.5",
-                                                "5",
-                                                if_else(time == "change.6",
-                                                        "6", time))))),
-         Supplement = if_else(supplement == "glucose",
-                              "Glucose",
-                              if_else(supplement == "placebo",
-                                      "Placebo", supplement))) %>%
-  select(Supplement, Time, emmean, lower.CL, upper.CL)
+vol.summarised <- vol.clean  %>%
+  group_by(time, supplement) %>%
+  summarise(mean.vol = mean(tot.volume, na.rm = TRUE),
+            sd.vol = sd(tot.volume, na.rm = TRUE)) %>%
+  
+  print()
 
 
-# Flextable
+# 
+vol.ch <- vol.change %>%
+  group_by(time, supplement) %>%
+  summarise(mean.change = mean(change, na.rm = TRUE),
+            sd.change = sd(change, na.rm = TRUE)) %>%
+  ungroup() %>%
+  add_row(time = "baseline", supplement = "placebo", mean.change = 0, sd.change = 0, .before = 1) %>%
+  add_row(time = "baseline", supplement = "glucose", mean.change = 0, sd.change = 0, .before = 2) %>%
+  mutate(time = if_else(time == "baseline",
+                         "baseline",
+                         if_else(time == "change.2",
+                                 "session2",
+                                 if_else(time == "change.3",
+                                         "session3",
+                                         if_else(time == "change.4",
+                                                 "session4",
+                                                 if_else(time == "change.5",
+                                                         "session5",
+                                                         if_else(time == "change.6",
+                                                                 "session6", time))))))) %>%
+  print()
+
+
+joined.vol <- vol.summarised %>%
+  full_join(vol.ch) %>%
+  mutate(time = if_else(time == "baseline",
+                        "1",
+                        if_else(time == "session2",
+                                "2",
+                                if_else(time == "session3",
+                                        "3",
+                                        if_else(time == "session4",
+                                                "4",
+                                                if_else(time == "session5",
+                                                        "5",
+                                                        if_else(time == "session6",
+                                                                "6", time)))))),
+         supplement = if_else(supplement == "placebo",
+                              "Placebo",
+                              if_else(supplement == "glucose",
+                                      "Glucose", supplement)))
+  print()
+
+# Flextable for change
 
 set_flextable_defaults(
   font.size = 10, theme_fun = theme_booktabs,
   padding = 6,
   background.color = "#EFEFEF")
 
-vol.tab <- flextable(vol.emm)
-
-
-vol.tab <- width(vol.tab, width = 1.1)
+vol.tab <- flextable(joined.vol)
 
 vol.tab <- colformat_double(vol.tab,
                  big.mark=",", digits = 2, na_str = "N/A")
 
+vol.tab <- width(vol.tab, width = 1.02)
 
 vol.tab <- set_header_labels(vol.tab,
-                             Supplement = "Supplement", Time = "Session", emmean = "Total volume change (kg)",
-                             lower.CL = "Lower", upper.CL = "Upper")
+                             time = "Session", supplement = "Supplement", mean.vol = "Avg. volume (kg)",
+                             sd.vol = "SD", mean.change = "Avg. change (kg)", sd.change = "SD")
 
 
 vol.tab
 
+#################################### OLD CODE BELOW ##############################################
 
+# Model
 
-
-
+#m <- lmerTest::lmer(change ~ 0 + baseline + time + supplement:time + (1|subject),
+#                     data = vol.change)
+#plot(m)
+#msum <- summary(m)
+#
+#vol.emm <- confint(emmeans(m, specs = ~"supplement|time")) %>%
+#  data.frame()
+#
+#saveRDS(confint.m2, "./data/data-gen/training/vol.emm.RDS")
+#
+#vol.emm <- readRDS("./data/data-gen/training/vol.emm.RDS") %>%
+#  select(-SE, -df) %>%
+#  mutate(Time = if_else(time == "change.2",
+#                        "2",
+#                        if_else(time == "change.3",
+#                                "3",
+#                                if_else(time == "change.4",
+#                                        "4",
+#                                        if_else(time == "change.5",
+#                                                "5",
+#                                                if_else(time == "change.6",
+#                                                        "6", time))))),
+#         Supplement = if_else(supplement == "glucose",
+#                              "Glucose",
+#                              if_else(supplement == "placebo",
+#                                      "Placebo", supplement))) %>%
+#  select(Supplement, Time, emmean, lower.CL, upper.CL)
 
 
 
