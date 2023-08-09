63829ddc4a8990759fd5a870082fee513abd14eb
Kristianlian <74902710+Kristianlian@users.noreply.github.com>
2023-08-09 10:05:11 +0200

    table and template updates

--- figures/primer.tab.R
+++ figures/primer.tab.R
@@ -13,15 +13,25 @@ tab.dat <- primer.dat %>%
 
 
 ## Flextable
-tab.dat %>%
+primer.tab <- tab.dat %>%
   flextable() %>%
   compose(i = 1, j = c(1, 2, 3, 4), part = "header",
           value = c(as_paragraph("Gene"),
                     as_paragraph("Sequence (forward - reverse)"),
                     as_paragraph("CT mean (SD)"),
                     as_paragraph("E"))) %>%
-  #add_header_row(values = c(" ", " ", " ", "Primer sequences"), 
-               #  colwidths = c(1, 1, 1, 1)) %>%
-  set_caption("Table 2. Primer Sequences") %>%
+  set_caption("Table 2. Primer Sequences")
   set_table_properties(layout = "autofit")
+  
+
+
+primer.tab <- border(primer.tab, 
+                     i = NULL,
+                     j = NULL,
+                     border = NULL,
+                     border.top = fp_border_default(color = "grey"),
+                     border.bottom = fp_border_default(color = "grey"),
+                     border.left = fp_border_default(color = "grey"),
+                     border.right = fp_border_default(color = "grey"),
+                     part = "all")
 
