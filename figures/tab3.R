50011a11aaf0595f5b71d47dcee984ff3f133581
Kristian Lian <kristian.lian@inn.no>
2024-01-09 08:10:04 +0100

    finished humac figure

--- figures/tab3.R
+++ figures/tab3.R
@@ -25,26 +25,14 @@ isometric.acute <- readRDS("./data/data-gen/humac/isom.absolutechange.ac.RDS")
 iso60.acute <- readRDS("./data/data-gen/humac/iso60.absolutechange.ac.RDS")
 iso240.acute <- readRDS("./data/data-gen/humac/iso240.absolutechange.ac.RDS")
 
-
-# Joining data
-
-rest.change <- isometric |>
-  full_join(iso60) |>
-  full_join(iso240)
-
-acute.change <- isometric.acute |>
-  full_join(iso60.acute) |>
-  full_join(iso240.acute)
-
-
 ## Summarising
 
-# Baseline values
+# Wide format
 
 tabdat <- humac.clean2 |>
   select(subject, supplement, time, acute, measure = test, peak.torque) %>%
 #  filter(time == "baseline" & acute == "rest") |>
-  group_by(supplement, measure, time) |>
+  group_by(measure, supplement, time) |>
   summarise(mean.pt = mean(peak.torque),
             sd.pt = sd(peak.torque)) |>
   mutate(mean = paste0(sprintf("%.1f",mean.pt), " (", sprintf("%.1f",sd.pt), ")")) |>
@@ -53,28 +41,31 @@ tabdat <- humac.clean2 |>
                        "Glucose",
                        if_else(supplement == "placebo",
                                "Placebo", supplement))) |>
-  mutate(measure = if_else(measure == "isok.240",
-                           "240",
+  mutate(measure = if_else(measure == "isom",
+                           "0",
                            if_else(measure == "isok.60",
                                    "60",
-                                   if_else(measure == "isom",
-                                           "0", measure)))) |>
+                                   if_else(measure == "isok.240",
+                                           "240", measure)))) |>
   pivot_wider(names_from = time,
               values_from = mean)
 
 
-humac.flex <- tabdat |>
+## Making the table
+
+humac.flex <- tabdat.wide |>
   flextable() |>
   compose(i = 1, j = c(1, 2, 3, 4, 5, 6, 7, 8, 9), part = "header",
           value = c(as_paragraph("Condition"),
                     as_paragraph("Velocity"),
                     as_paragraph("Baseline"),
-                    as_paragraph("23h after 2RT"),
-                    as_paragraph("23h after 4RT"),
-                    as_paragraph("23h after 5RT"),
-                    as_paragraph("30min after 6RT"),
-                    as_paragraph("2h after 6RT"),
-                    as_paragraph("23h after 6RT"))) 
+                    as_paragraph("After 2RT"),
+                    as_paragraph("After 4RT"),
+                    as_paragraph("After 5RT"),
+                    as_paragraph("After 6RT"),
+                    as_paragraph("After 6RT"),
+                    as_paragraph("After 6RT"))) |>
+  add_body_row(values = c(" ", "Knee-extension peak torque (Nm)"), colwidths = c(2, 7))
 
 humac.flex <- border(humac.flex, 
                      i = NULL,
@@ -86,13 +77,28 @@ humac.flex <- border(humac.flex,
                      border.right = fp_border_default(color = "black"),
                      part = "all") 
 
-
+humac.flex <- footnote(humac.flex,
+                       i = 4,
+                       j = 6,
+                       as_paragraph("Sig. difference"),
+                       ref_symbols = "*",
+                       part = "body", inline = TRUE)
 
 
 
 
 
 ############################### OLD CODE BELOW #################################################
+
+# Joining data
+
+#rest.change <- isometric |>
+ # full_join(iso60) |>
+  #full_join(iso240)
+
+#acute.change <- isometric.acute |>
+ # full_join(iso60.acute) |>
+  #full_join(iso240.acute)
 
 # Pre-post session five change (rest)
 
