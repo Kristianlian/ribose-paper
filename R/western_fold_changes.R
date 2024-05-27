3e2157706fd077366f087d13e252438fd4a6c601
Kristian <kristian.lian@inn.no>
2024-05-27 09:52:37 +0200

    wrote xlsx

--- R/western_fold_changes.R
+++ R/western_fold_changes.R
@@ -3,7 +3,7 @@
 
 
 # Packages
-library(tidyverse); library(emmeans); library(lme4); library(buildmer)
+library(tidyverse); library(emmeans); library(lme4); library(buildmer); library(writexl)
 
 #
 # Data 
@@ -15,16 +15,29 @@ prot.dat <- readRDS("./data/data-gen/protein/prot.dat.RDS") %>%
 
 
 
-prot.dat %>%
+prot.raw <- prot.dat %>%
   select(-sample.id, -gel.sample) %>%
   pivot_wider(names_from = time, 
               values_from = norm.sign) %>%
-  mutate(fc = post / pre) %>%
-  
+  mutate(fc = post / pre) 
+
+write_xlsx(prot.raw, "./data/data-gen/prot.raw.xlsx")
+
+prot.fc <- prot.raw %>%
   group_by(subject, leg, target, supplement) %>%
   summarise(sd.fc = sd(fc), 
-          mean.fc = mean(fc)) %>%
+          mean.fc = mean(fc))
+
+write_xlsx(prot.fc, "./data/data-gen/prot.fc.xlsx")
+
+
+myc.fc <- prot.fc %>%
+  filter(target == "cmyc")
+
+write_xlsx(myc.fc, "./data/data-gen/myc.fc.xlsx")
   
+
+prot.fc %>%
   ggplot(aes(supplement, mean.fc)) + geom_point() + facet_wrap(~ target)
   
   
